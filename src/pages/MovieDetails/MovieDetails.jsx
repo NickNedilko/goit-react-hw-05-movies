b9f711a9a741bbebf81ac3f274eca23ae09b6fd9
NickNedilko <overlord311089@gmail.com>
2023-05-22 18:47:30 +0300

    work5

---
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,32 @@
+import FilmInfo from 'components/FilmInfo/FilmInfo';
+import React, { useState, useEffect, Suspense } from 'react';
+import { useParams, Outlet, useLocation } from 'react-router-dom';
+import { apiFilmById } from 'services/ApiFilms';
+
+const MovieDetails = () => {
+  const [filmInfo, setFilmInfo] = useState(null);
+  const { filmId } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!filmId) {
+      return;
+    }
+    apiFilmById(filmId).then(({ data }) => {
+      setFilmInfo(data);
+    });
+  }, []);
+
+  return (
+    filmInfo && (
+      <>
+        <FilmInfo filmInfo={filmInfo} location={location} />
+        <Suspense fallback={<div>Loading.......</div>}>
+          <Outlet />
+        </Suspense>
+      </>
+    )
+  );
+};
+
+export default MovieDetails;
